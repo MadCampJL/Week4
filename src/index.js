c7b89525db0e18a281d242c9087bc181846ff0f1
Seungwon Ju <dshseungwon@gmail.com>
2019-01-19 16:18:12 +0900

    Firebase Integration

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import "assets/css/material-dashboard-react.css?v=1.5.0";
 
 import indexRoutes from "routes/index.jsx";
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={hist}>
+  <HashRouter>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return <Route path={prop.path} component={prop.component} key={key} />;
       })}
     </Switch>
-  </Router>,
+  </HashRouter>,
   document.getElementById("root")
 );
